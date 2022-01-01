bac1e409399712516bf1910da6338bf806d5af31
shedrack <shedrackabel900@gmail.com>
2022-01-01 14:40:27 -0800

    done

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 // mod print;
 // mod variables;
-mod types;
+// mod types;
+mod strings;
 
 fn main() {
     //    print::run();
     //    variables::run();
-    types::run();
+    // types::run();
+    strings::run();
 }
--- src/types.rs
+++ src/types.rs
@@ -1,1 +1,24 @@
-pub fn run() {}
+pub fn run() {
+    let x = 1;
+    println!("{}", x);
+
+    let y = 2.5;
+    println!("{}", y);
+
+    let large: i64 = 4545454545454546;
+    println!("{}", large);
+    println!("Max i32: {}", std::i32::MAX);
+    println!("Max i64: {}", std::i64::MAX);
+    println!("Max f32: {}", std::f32::MAX);
+    println!("Max f64: {}", std::f64::MAX);
+
+    let is_married = true;
+    let is_greater = 20 > 10;
+    let emojis = 'A';
+    let face = '\u{1F600}';
+    let new_face = '😎';
+    println!(
+        "Length: {:?}",
+        (x, y, is_married, is_greater, emojis, face, new_face)
+    );
+}
