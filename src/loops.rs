1f5d90252b049ee79e272e8b552d3065f3b564c5
shedrack <shedrackabel900@gmail.com>
2022-01-02 02:15:41 -0800

    done

--- src/loops.rs
+++ src/loops.rs
@@ -1,1 +1,40 @@
-pub fn my_loop() {}
+pub fn my_loop() {
+    let mut count = 0;
+
+    // Infinite loop
+    loop {
+        count += 1;
+        println!("Number: {}", count);
+
+        if count == 100 {
+            break;
+        }
+    }
+
+    //While loop
+    while count <= 100 {
+        if count % 15 == 0 {
+            println!("FizzBuzz");
+        } else if count % 3 == 0 {
+            println!("Fizz");
+        } else if count % 5 == 0 {
+            println!("Buzz");
+        } else {
+            println!("Count: {}", count);
+        }
+
+        count += 1;
+    }
+
+    for x in 0..100 {
+        if x % 15 == 0 {
+            println!("FizzBuzz");
+        } else if x % 3 == 0 {
+            println!("Fizz");
+        } else if x % 5 == 0 {
+            println!("Buzz");
+        } else {
+            println!("Count: {}", x);
+        }
+    }
+}
